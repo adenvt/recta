bfa0a38371d035e8694803483033338afc3975af
adenvt <adenvt@gmail.com>
2017-08-24 22:46:14 +0700

    add karma, mocha, chai

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-expressions */
+describe('Recta', function () {
+  it('Should be defined', () => {
+    Recta.should.not.be.undefined
+  })
+
+  it('Should be connected to server', () => {
+    const printer = new Recta('123456789', 1811)
+
+    return printer.open().then(() => {
+      printer.isOpen.should.be.true
+    })
+  })
+})
